f19075f40ff4ca5e52e3a34f96c4c036f76f3e40
igor.ribeiro <igorapalvesribeiro@gmail.com>
2022-09-06 22:14:45 -0300

    fundamentos - Estilizando Componentes

--- App.tsx
+++ App.tsx
@@ -2,9 +2,28 @@ import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <Text>React</Text>
-      <Text>Native</Text>
+    <View style={{
+      flex: 1,
+      backgroundColor: '#131016',
+      padding: 24
+    }}>
+      <Text
+        style={{
+          color: '#fdfcfe',
+          fontSize: 24,
+          fontWeight: 'bold',
+          marginTop: 48
+        }}
+      >Nome do Evento
+      </Text>
+      <Text
+        style={{
+          color: '#6b6b6b',
+          fontSize: 16
+        }}
+      >
+        Sexta,4 de Novembro de 2022
+      </Text>
     </View>
   );
 }
