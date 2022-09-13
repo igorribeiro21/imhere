89cf7bbb05b854f30506f137546c64bbe4d08246
igor.ribeiro <igorapalvesribeiro@gmail.com>
2022-09-12 21:57:03 -0300

    fundamentos - Adicionando TextInput e TouchableOpacity

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,12 @@
-import { Text, View } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 import { styles } from './styles';
 
 export default function Home() {
+
+    function handleParticipantAdd() {
+        console.log('Você clicou no botão');
+    }
+
     return (
         <View style={styles.container}>
             <Text
@@ -13,6 +18,21 @@ export default function Home() {
             >
                 Sexta,4 de Novembro de 2022
             </Text>
+
+            <TextInput
+                style={styles.input}
+                placeholder="Nome do participante"
+                placeholderTextColor={"#6b6b6b"}
+            />
+
+            <TouchableOpacity
+                style={styles.button}
+                onPress={handleParticipantAdd}
+            >
+                <Text style={styles.buttonText}>
+                   +
+                </Text>
+            </TouchableOpacity>
         </View>
     );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -7,7 +7,7 @@ export const styles = StyleSheet.create({
         padding: 24
     },
     eventName: {
-        color: '#fdfcfe',
+        color: '#fff',
         fontSize: 24,
         fontWeight: 'bold',
         marginTop: 48
@@ -15,5 +15,25 @@ export const styles = StyleSheet.create({
     eventDate: {
         color: '#6b6b6b',
         fontSize: 16
+    },
+    input: {
+        height: 56,
+        backgroundColor: '#1f1e25',
+        borderRadius: 5,
+        color: '#fff',
+        padding: 16,
+        fontSize: 16
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 30,        
+    },
+    button: {
+        width: 56,
+        height: 56,
+        borderRadius: 5,
+        backgroundColor: '#31cf67',
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 });
